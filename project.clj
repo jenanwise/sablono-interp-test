af3e9147832ac14a441c5cabfd530f04a570c805
Jenan Wise <jenanwise@gmail.com>
2014-07-29 11:11:09 -0700

    Match release version of sablono.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2277"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
-                 [sablono "0.2.19-SNAPSHOT"]
+                 [sablono "0.2.18"]
                  [om "0.6.5"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]]
